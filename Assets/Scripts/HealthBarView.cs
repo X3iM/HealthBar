9c24606e186b5528ced641942a5ea2c1f7c897e9
Artem <atremgrinenko8@gmail.com>
2023-07-30 11:20:49 +0200

    fix

--- Assets/Scripts/HealthBarView.cs
+++ Assets/Scripts/HealthBarView.cs
@@ -6,13 +6,13 @@ public class HealthBarView : MonoBehaviour
     [SerializeField] private Slider _slider;
     [SerializeField] private Button _damage;
     [SerializeField] private Button _heal;
-    [SerializeField] private float _playerHealth = 100f;
+    [SerializeField] private float _maxPlayerHealth = 100f;
 
     private Player _player;
 
     private void Start()
     {
-        _player = new Player(_playerHealth);
+        _player = new Player(_maxPlayerHealth);
         _damage.onClick.AddListener(Damage);
         _heal.onClick.AddListener(Heal);
         _slider.value = 1;
@@ -30,9 +30,14 @@ public class HealthBarView : MonoBehaviour
         CountSliderValue();
     }
 
+    private void FixedUpdate()
+    {
+        CountSliderValue();
+    }
+
     private void CountSliderValue()
     {
         float health = _player.Health / _player.MaxHealth;
-        _slider.value = Mathf.MoveTowards(_slider.value, health, 1);
+        _slider.value = Mathf.MoveTowards(_slider.value, health, Time.deltaTime);
     }
 }
